4499dbd7205a36a4ee0c2c35b2e84dc1b9e6427a
davidazevedo <davidengine@gmail.com>
2023-02-20 20:46:28 +0000

    Update 3 files

--- src/app/conector-websocket/conector-websocket.component.ts
+++ src/app/conector-websocket/conector-websocket.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {   Component,
+  Injector,
+  OnInit,
+  Output,
+  EventEmitter } from '@angular/core';
+import { PushDto } from '../shared/PushDto';
+import { HttpClient } from '@angular/common/http';
+import { WebSocketConnector } from '../shared/websocket-connector';
 
 @Component({
   selector: 'app-conector-websocket',
   templateUrl: './conector-websocket.component.html',
   styleUrls: ['./conector-websocket.component.css']
 })
-export class ConectorWebsocketComponent {
+export class ConectorWebsocketComponent  { 
+  constructor(private http: HttpClient) {
+  }
 
+      items: any[] = [];
+      push: PushDto = new PushDto();
+      private webSocketConnector: WebSocketConnector;
+
+      connect(): void {
+        this.webSocketConnector = new WebSocketConnector(
+          this.push.IP,
+          this.push.Message,
+          this.onMessage.bind(this)
+        );
+      }
+     
+      onStart(message: any): void {
+        this.items.push(message.body);
+      }
+
+      onStop(message: any): void {
+        this.items.push(message.body);
+      }
+
+      onMessage(message: any): void {
+        this.items.push(message.body);
+      }  
 }
